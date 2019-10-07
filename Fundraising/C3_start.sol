6804b57adef8c0023436e306adb4ca4364e19872
maxwellfoley3 <52667841+maxwellfoley3@users.noreply.github.com>
2019-10-07 15:02:54 -0400

    Update C3_start.sol

--- Fundraising/C3_start.sol
+++ Fundraising/C3_start.sol
@@ -2,18 +2,12 @@ pragma solidity ^0.5.10;
 
 contract FundRaising {
 
-  address payable owner;
-  uint public goal;
-  uint public endTime;
-  uint public totalContribution = 0;
-
   mapping(address=>uint) donations;
 
   // add constructor function here
 
   function add() public payable {
     donations[msg.sender] += msg.value;
-    totalContribution += msg.value;
   }
 
   // add withdraw function here
