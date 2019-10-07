635b8c77e9bce739251788e4582cf98ac0f48527
maxwellfoley3 <52667841+maxwellfoley3@users.noreply.github.com>
2019-10-07 15:08:08 -0400

    Update C5_start.sol

--- Fundraising/C5_start.sol
+++ Fundraising/C5_start.sol
@@ -5,9 +5,9 @@ contract FundRaising {
   address payable owner;
   uint public goal;
   uint public endTime;
-  uint public totalContribution = 0;
 
   mapping(address=>uint) donations;
+  address[] donators; 
 
   constructor(uint _goal, uint _timelimit) public {
     owner = msg.sender;
@@ -16,17 +16,23 @@ contract FundRaising {
   }
 
   function add() public payable {
+    if(!donations[msg.sender]) {
+      donators.push(msg.sender);
+    }
     donations[msg.sender] += msg.value;
     totalContribution += msg.value;
   }
 
   function withdrawOwner() public {
-    require(totalContribution >= goal, "Fundraising not closed yet");
+    require(address(this).balance >= goal, "Fundraising not closed yet");
     require(msg.sender == owner, "You must be the owner");
     owner.transfer(address(this).balance);
   }
-
+  
+  //WRONG!! Don't do this!!
   function withdraw() public {
-
-  }
+    for(uint i = 0; i < donators.length; i++) {
+      donators[i].transfer(donations[donators[i]]);
+    }
+  } 
 }
