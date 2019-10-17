a6ec8488a1eadca36a6fbde4403713fcc72a1d28
maxwellfoley3 <52667841+maxwellfoley3@users.noreply.github.com>
2019-10-17 14:48:38 -0400

    removed view from functions where gas cost is part of the tutorial (#17)

--- Gas/C5_start.sol
+++ Gas/C5_start.sol
@@ -4,19 +4,19 @@ contract StringTest {
   string a = "fsdf€bÁ";
   bytes b = "fsdf€bÁ";
 
-  function gets() public view returns(string memory) {
+  function gets() public returns(string memory) {
     return a;
   }
 
-  function getb() public view returns(bytes memory) {
+  function getb() public returns(bytes memory) {
     return b;
   }
 
-  function getl() public view returns(uint) {
+  function getl() public returns(uint) {
     return bytes(a).length;
   }
 
-  function getlb() public view returns(uint) {
+  function getlb() public returns(uint) {
     return b.length;
   }
 }
